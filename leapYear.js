38d4bf2690fb4886966af93f813c1b8a197dbffb
Michael Spremulli <michaels.wpi@gmail.com>
2020-06-04 12:04:13 -0400

    added error handling to leap year algorithm

--- leapYear.js
+++ leapYear.js
@@ -1,12 +1,18 @@
 //checks for leap year yet
 function isLeapYear(year){
-  let resultText = (year % 4 == 0 && year % 100 !=0) || year % 400 == 0  ? "is" : "is not";
-  let isLeapYear = (year % 4 == 0 && year % 100 !=0) || year % 400 == 0 ? true : false;
-  console.log(`${year} ${resultText} a leap year.`);
-  return isLeapYear;
+  if(Number.isInteger(year) && year >=0 ){
+    let isLeapYear = (year % 4 === 0 && year % 100 !==0) || year % 400 === 0 ? true : false;
+    let resultText = isLeapYear  ? "is" : "is not";
+    console.log(`${year} ${resultText} a leap year.`);
+    return isLeapYear;
+    }
+  console.error(`${year} is not a valid year.`)
+ 
 }
 
 isLeapYear(1900);
 isLeapYear(2000);
 isLeapYear(2004);
 isLeapYear(1600);
+isLeapYear(1600.5);
+isLeapYear("1600");
