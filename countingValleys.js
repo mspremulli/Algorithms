dcde884c41ff2052385006eef1686b2878434ab3
Michael Spremulli <michaels.wpi@gmail.com>
2020-06-19 04:37:29 -0400

    added counting valleys algorithm

---
+++ countingValleys.js
@@ -0,0 +1,18 @@
+// Complete the countingValleys function below.
+const countingValleys = (n, s) => {
+  let  altitude = 0,
+      numValley = 0,
+      inValley = false;
+  s = s.split('');
+
+  s.forEach(step => {
+    altitude = step ==='U' ? altitude + 1 : altitude - 1
+    numValley = inValley && altitude >= 0 ? numValley + 1 : numValley;
+    inValley = altitude < 0 ? true : false;
+    
+  });
+  return numValley;
+
+}
+
+console.log(countingValleys(8,'UDDDUDUU'));
