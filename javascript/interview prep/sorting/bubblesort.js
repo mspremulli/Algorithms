16cad153ca8a63a96b2f7fa376d616c9e0d5b5eb
Michael Spremulli <michaels.wpi@gmail.com>
2020-06-23 19:53:02 -0400

    added bubblesort counting excercise

---
+++ javascript/interview prep/sorting/bubblesort.js
@@ -0,0 +1,25 @@
+// Complete the countSwaps function below.
+const countSwaps = (a) => {
+  console.log(a);
+  
+  let numSwaps = 0;
+
+  for(let i = 0; i < a.length; i++) {
+   for(let j = 0; j < a.length - 1; j++) {
+      // Swap adjacent elements if they are in decreasing order
+      if (a[j] > a[j + 1]) {
+        let temp = a[j];
+        a[j] = a[j+1];
+        a[j+1] = temp;
+        numSwaps++;
+      }
+   }
+  }
+
+  console.log(`Array is sorted in ${numSwaps} swaps. `);
+  console.log(`First Element: ${a[0]} `);
+  console.log(`Last Element: ${a[a.length-1]}`);
+
+}
+
+countSwaps([1,2,4,5,3,6,8,9]);
