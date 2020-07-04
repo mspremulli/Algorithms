8babce28d26e6da991153eb4ba4ddb9d5fe3350b
Michael Spremulli <michaels.wpi@gmail.com>
2020-07-04 15:08:22 -0400

    even fibonacci sum algorithm

--- javascript/unfinished/evenFibonacci.js
+++ javascript/unfinished/evenFibonacci.js
@@ -3,8 +3,26 @@
 
 function fiboEvenSum(n) {
     // You can do it!
-    return true;
+  let i = 0,
+      sum = 0;
+  while(getFib(i) <= n){
+    let current = getFib(i)
+    sum = current % 2 === 0 ? sum + current : sum;
+    i++
   }
+  console.log(sum);
+}
+
+//get the current fibonocci number
+function getFib(index) {
+
+  if(index > 1){
+    return(getFib(index-1) + getFib(index-2));
+  }
+  else return 1;
+
+}
   
   fiboEvenSum(10);
+  fiboEvenSum(5);
   
