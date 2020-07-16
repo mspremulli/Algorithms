effe86eac290e6f93fbcbaf3ff322c55c39d9cc4
Michael Spremulli <michaels.wpi@gmail.com>
2020-07-16 11:40:47 -0400

    angry professor algorithm

---
+++ angryProfessor.js
@@ -0,0 +1,14 @@
+// Complete the angryProfessor function below.
+const angryProfessor = (threshold, studentArrivalArray) => {
+  let numberofStudentsOnTime = 0;
+  studentArrivalArray.forEach(student => {
+    if(student <= 0) numberofStudentsOnTime++;
+  });
+  // console.log(numberofStudentsOnTime, threshold);
+
+  if(numberofStudentsOnTime >= threshold) return 'NO';
+  return 'YES';
+}
+
+console.log(angryProfessor(3, [1,0,-3,-4]));
+console.log(angryProfessor(2, [1,0,3,4]));
