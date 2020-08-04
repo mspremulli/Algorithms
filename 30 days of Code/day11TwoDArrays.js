f6f030266a4c0ea534aea3e296099cefe0ba5d53
Michael Spremulli <michaels.wpi@gmail.com>
2020-08-04 03:05:43 -0400

    optimized 2D array algorithm

--- 30 days of Code/day11TwoDArrays.js
+++ 30 days of Code/day11TwoDArrays.js
@@ -1,19 +1,18 @@
 // Complete the hourglassSum function below.
 const  hourglassSum = (arr) => {
-  let highestSum = -99999999
-  let hourglassSum = 0;
-
-    for(let i=0;i<4;i++){
-      for(let j=0;j<4;j++){
+  let highestSum = -99999999,
+      hourglassSum = 0,
+      arrLength = arr[0].length - 2;
+    //loop through each starting point for the hourglass
+    for(let i = 0; i < arrLength; i++){
+      for(let j = 0; j < arrLength; j++){
+      //calculate the sum of the hourglass and update the maximum sum so far
       hourglassSum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
-      // console.log(hourglassSum);
       highestSum = (hourglassSum > highestSum) ? hourglassSum: highestSum;
-      
     }
   }
   return highestSum;
 }
-
 
 console.log(hourglassSum([
   [1, 1, 1, 0, 0, 0]
