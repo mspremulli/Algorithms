f66eceae6c63eaddf5eaa69233c52489798aedd6
Michael Spremulli <michaels.wpi@gmail.com>
2020-08-14 21:42:43 -0400

    optimized algorithm and cleaned up code

--- iceCreamParlor.js
+++ iceCreamParlor.js
@@ -1,31 +1,34 @@
 // Complete the whatFlavors function below.
-function whatFlavors(costs, budget) {
-  //create hastable
-  let iceCreamHashTable = [];
-  costs.forEach((cost, index) => {
-    if(iceCreamHashTable[cost]) iceCreamHashTable[cost].push(index + 1);
-    else iceCreamHashTable[cost] = [index + 1];
-  });
-  // console.log(iceCreamHashTable);
-
+function whatFlavors(prices, budget) {
   let countIndex = 0;
   let iceCreamIndex;
-  while(costs.length > 0){
+  let iceCreamCost;
+  let iceCreamHashTable = [];
+  //create hastable
+  prices.forEach((price, index) => {
+    if(iceCreamHashTable[price]) iceCreamHashTable[price].push(index + 1);
+    else iceCreamHashTable[price] = [index + 1];
+  });
+
+  // remove the case where a single ice cream is chosen twice
+  if(budget % 2 === 0 && iceCreamHashTable[budget/2].length === 1){
+   iceCreamHashTable.splice(budget/2,1,null);
+  }
+
+  //loop through the prices until a match is found
+  while(prices.length > 0){
     //remove item being checked from the array
-    let iceCreamCost = costs.splice(0, 1);
-    // console.log(iceCreamHashTable[iceCreamCost[0]].length);
-    if(!(iceCreamCost[0] === budget/2 && iceCreamHashTable[iceCreamCost[0]].length < 2)){
-      iceCreamIndex = iceCreamHashTable[budget - iceCreamCost];
-    }
+    iceCreamCost = prices.splice(0, 1)[0];
+    iceCreamIndex = iceCreamHashTable[budget - iceCreamCost];
     countIndex++;
     if (iceCreamIndex){
-      console.log(countIndex, iceCreamIndex[iceCreamIndex.length - 1]);
+       console.log(countIndex, iceCreamIndex[iceCreamIndex.length - 1]);
       return;
     }
   }
 }
 
-// whatFlavors([2, 2, 4, 3], 4);
-// whatFlavors([45, 65, 56, 1, 5, 4, 3, 6], 5);
+whatFlavors([2, 2, 4, 3], 4);
+whatFlavors([45, 65, 56, 1, 5, 4, 3, 6], 5);
 whatFlavors([4, 3, 2, 5, 7], 8);
 
