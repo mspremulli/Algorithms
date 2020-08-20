bbba4150eee470aed4e28f864c19ee6b143ad7fe
Michael Spremulli <michaels.wpi@gmail.com>
2020-08-20 15:28:27 -0400

    switch object to a hash table to speed up algorithm

--- minMaxRiddle.js
+++ minMaxRiddle.js
@@ -23,25 +23,30 @@ function riddle(arr) {
   });
   // console.log(minValueDistance);
 
+  //transfer object to a hash table
+  let hashTable = [];
+  minValueDistance.forEach(item => {
+    hashTable[item.distance] = !hashTable[item.distance] ? item.value : Math.max(hashTable[item.distance],item.value);
 
-  // build maximum object
-  let maxDistance = [],
-      maxValue;
-  for(let windowSize = 1; windowSize <= arr.length; windowSize++){
-    maxValue = 0;
-    minValueDistance.forEach(item => {
-      if(item.value > maxValue && item.distance >= windowSize){
-        maxValue = item.value;
-      }
-    });
-    maxDistance.push(maxValue);
+    // if(!hashTable[item.distance]){
+    //   hashTable[item.distance] = item.value;
+    // }
+    // else{
+    //   hashTable[item.distance] = Math.max(hashTable[item.distance],item.value);
+    // }
+  });
+
+  //loop through window and add update the hashtable value until its a full array
+  for(let i = arr.length; i > 0  ; i--){
+    if(!hashTable[i] || hashTable[i] < hashTable[i+1]) hashTable[i] = hashTable[i+1];
   }
 
-  return maxDistance;
+  hashTable.shift()
+  return hashTable;
 }
 
-// console.log(riddle([3, 5, 4, 7, 6, 2]));
-// console.log(riddle([6,3,5,1,12]));
+console.log(riddle([3, 5, 4, 7, 6, 2]));
+console.log(riddle([6,3,5,1,12]));
 console.log(riddle([789168277,694294362,532144299,20472621,316665904,59654039,685958445,925819184,371690486,285650353,522515445,624800694,396417773,467681822,964079876,355847868,424895284,50621903,728094833,535436067,221600465,832169804,641711594,518285605,235027997,904664230,223080251,337085579,5125280,448775176,831453463,550142629,822686012,555190916,911857735,144603739,751265137,274554418,450666269,984349810,716998518,949717950,313190920,600769443,140712186,218387168,416515873,194487510,149671312,241556542,575727819,873823206]));
 
 
